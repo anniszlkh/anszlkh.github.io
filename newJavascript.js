f7f7951b3a3514705473caa733e97d54187dc702
anniszlkh <152512491+anniszlkh@users.noreply.github.com>
2023-12-01 00:51:23 +0800

    Add files via upload

---
+++ newJavascript.js
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  // Add your form submission logic here
+});
